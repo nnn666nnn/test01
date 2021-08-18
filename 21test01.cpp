dfa991417e7ea37026e8cf9285710e203d5b148e
unknown <nnn666nnn@126.com>
2021-08-18 12:09:18 +0800

    two

---
+++ 21test01.cpp
@@ -0,0 +1,19 @@
+﻿// 21test01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include<string>
+using namespace std;
+void TestWork() {
+    cout << "Please write your name" << endl;
+    string yourName = "";
+    cin >> yourName;
+    cout << "fisajdfijsidfjiosjisji" << yourName << endl;
+}
+int main()
+{
+    TestWork();
+    return 0;
+}
+
+
